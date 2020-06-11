260b9454d4475591f01ef61ce8c5e0f86df3b63c
rain2002kr <58737802+rain2002kr@users.noreply.github.com>
2020-06-11 19:23:30 +0900

    create form submit 추가

--- src/components/CreateContent.js
+++ src/components/CreateContent.js
@@ -7,6 +7,15 @@ class CreateContent extends Component {
           <article>
             <h1>{this.props.title}</h1>
             <p>{this.props.sub}</p>
+            <form action="/create" method="post" onSubmit = {function(e){
+              e.preventDefault(); }.bind(this)}>
+              
+              <p><input type="text" placeholder="title" name="title"></input></p>
+              
+              <p><textarea type="text" placeholder="dscriton" name="dsc"></textarea></p>
+              <p><input type="submit" value="만들기" name="btn-create" ></input></p>
+
+            </form>
           </article>
         </Fragment>
       );
